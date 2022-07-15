ce701d1e3f234030db48f832d9adca182dbdf9e9
Nitish Parihar <nitishp97@gmail.com>
2022-07-16 00:45:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-/**
-Challenge:
-
-1. Fetch a random image from the Dog API again 
-(https://dog.ceo/api/breeds/image/random)
-
-2. Access the DOM and insert the URL you got from the
-API as an image `src` property (probably easiest if 
-you create the image completely here in the JS and add 
-it as the innerHTML of another element on the DOM)
-*/
-
 let dogBtn = document.getElementById("dog-btn")
 
 let dom = document.getElementById("container")
@@ -24,4 +12,3 @@ function fetchData() {
 }
 
 dogBtn.addEventListener('click', function(){fetchData()})
-
