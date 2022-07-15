f754fd191e29cb5bb74a1050f716c0c006033f26
Nitish Parihar <nitishp97@gmail.com>
2022-07-16 00:46:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 let dogBtn = document.getElementById("dog-btn")
-
 let dom = document.getElementById("container")
 
 function fetchData() {
     fetch("https://dog.ceo/api/breeds/image/random")
     .then(response => response.json())
     .then(data => {
-        console.log(data)
         dom.innerHTML = `<img src="${data.message}" class="image"/>`
     })
 }
